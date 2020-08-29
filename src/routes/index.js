9098b8942f567c110ab8e2dca49f4b216b3ddda4
BugCode <bugcode3019@gmail.com>
2020-08-28 20:07:08 -0500

    Documentos HTML Completos

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index.html', { title: "Tienda" });
+    res.render('index.html', { title: "Novedades Max" });
+});
+
+router.get('/products', (req, res) => {
+    res.render('products.html', { title: "Novedades Max" });
+});
+
+router.get('/contact', (req, res) => {
+    res.render('contact.html', { title: "Novedades Max" });
 });
 
 module.exports = router;
